662162a6bd2b0361a224339ba8600f963de8321f
Alex Boylan <aboylan@users.noreply.github.com>
2024-10-26 20:33:46 -0600

    Los componentes y directivo ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola mundo Angular 18';
+  enabled: boolean = false;
+
+  setEnabled(): void{
+    this.enabled = this.enabled ? false : true;
+    console.log('hemos hecho click en setEnabled');
+  }
 }
