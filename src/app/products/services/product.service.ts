442b29a17bc897c0e561c02fd59b23c6eb15ffc4
Alex Boylan <aboylan@users.noreply.github.com>
2024-11-03 22:20:25 -0600

    Anadiendo el componente y service de Producto

---
+++ src/app/products/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private products: Product[] = [
+    {
+      id: 1,
+      name: 'Mesa comedor',
+      description: 'Excelente mesa para el comedor',
+      price: 700
+    },
+    {
+      id: 2,
+      name: 'Teclado mecanica',
+      description: 'Excelente teclado para typing',
+      price: 500
+    }
+  ];
+  
+  constructor() { }
+}
